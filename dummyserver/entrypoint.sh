7b4b0c7f5e5bc53e19fd4f8f4c6c022d443bcfca
Violet McKinney <vi@violet.wtf>
2022-06-18 23:48:04 -0700

    chore: fix GNU license link

--- dummyserver/entrypoint.sh
+++ dummyserver/entrypoint.sh
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https:#www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 touch index.html
 echo "<h1>this is dummy server $NUMBER" > index.html
--- scripts/make_echo_server.sh
+++ scripts/make_echo_server.sh
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https:#www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 docker rm -f "http-echo"
 docker run -d --name "http-echo" -p "8083:80" ealen/echo-server
